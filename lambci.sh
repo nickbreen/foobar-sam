727da0e2cacc6480261e5c433d0a4c8e69db8827
Nick Breen <nick@foobar.net.nz>
2019-04-21 11:12:42 +1200

    starting to upload artifact

--- lambci.sh
+++ lambci.sh
@@ -2,10 +2,19 @@
 
 set -ueo pipefail
 
+printenv
+
 composer_args="-vvv --no-interaction"
 composer="composer ${composer_args}"
 
+version=$(git describe --abbrev=0)
+
 . ~/init/php 7.3.3
-${composer} config version $(git describe --abbrev=0)
+${composer} config version ${version}
 ${composer} install --prefer-dist
 ${composer} archive
+
+
+#pip install awscli
+
+#aws s3 cp *-${version}.tar
