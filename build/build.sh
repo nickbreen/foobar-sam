238284c540ae38ca3b98b39cc3ad1b8e7048badd
Nick Breen <nick@foobar.net.nz>
2019-04-21 16:40:36 +1200

    error trap for printenv

--- build/build.sh
+++ build/build.sh
@@ -2,6 +2,8 @@
 
 set -ueo pipefail
 
+trap 'printenv' ERR
+
 composer_args="--no-interaction"
 composer="composer ${composer_args}"
 
@@ -17,6 +19,5 @@ declare BUCKET LAMBCI_REPO LAMBCI_BRANCH LAMBCI_BUILD_NUMBER
     cd build
     npm install
 )
-env
 key=artifacts/${LAMBCI_REPO}/${LAMBCI_BRANCH}/${LAMBCI_BUILD_NUMBER}
 node build/upload-artifacts.js ${BUCKET} ${key} *-${version}.tar
